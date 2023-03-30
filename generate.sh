e1db6165d813e055b0ba9c575e6d4b099fbac577
Aries Clark <me@ariesclark.com>
2023-03-29 18:57:33 -0600

    feat: add authentication patch

--- generate.sh
+++ generate.sh
@@ -28,8 +28,9 @@ sed -i "s/vr_chat/vrchat/g" ./.openapi-generator/FILES
 sed -i "s/Elixir/vrchat-elixir/" ./lib/vrchat/connection.ex
 
 find . -type f -name "*.ex" -exec sed -i "s/VRChat.Api./VRChat./g" {} \;
+find . -type f -name "*.ex" -exec sed -i "s/VRChat.Model.String/String/g" {} \;
 
-#git apply patches/authentication.patch
+git apply patches/authentication.patch
 
 mix deps.get
 mix format
